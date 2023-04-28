efe5ef9534c95faa694d715d4f1c9a683165c257
KorYme <maxime.corby.pro@gmail.com>
2023-04-28 18:04:39 +0200

    [ADD] : Some more tests

---
+++ Assets/SaveSystemClasses/DSM_GameData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KorYmeLibrary.SaveSystem 
+{
+   public class DSM_GameData : DataSaveManager<GameData>
+   {
+       // Modify if you're willing to add some behaviour to the component
+   }
+
+   [System.Serializable]
+   public class GameData : GameDataTemplate
+   {
+        // Create the values you want to save here
+        public SerializableDictionnary<int, bool> _testDictionnary;
+   }
+}
